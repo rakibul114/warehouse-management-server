e1b301d212e6f9434fc0234c73685c8634d2f435
rakibul114 <rakibulislam114@gmail.com>
2022-05-03 00:42:03 +0600

    loaded data to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +10,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// connection to MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2liqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    
+}
+
+run().catch(console.dir);
+
+
+// Root API
 app.get('/', (req, res) => {
     res.send('Running Warehouse Server');
 });
 
+
+// App Listener
 app.listen(port, () => {
     console.log('Listening to port', port);
 });
