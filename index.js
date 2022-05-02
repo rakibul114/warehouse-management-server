c9b124fda10786d6984e0bf5c0848fbbb9d23ba4
rakibul114 <rakibulislam114@gmail.com>
2022-05-03 01:11:05 +0600

    get single data from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -19,7 +19,29 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    
+    try {
+        await client.connect();
+        const productCollection = client.db('warehouseManagement').collection('product');
+
+        // API to get all products data
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        // API to get single product data
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+    }
+    finally {
+        
+    }
 }
 
 run().catch(console.dir);
