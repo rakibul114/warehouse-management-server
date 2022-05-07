cd407380ae5cdbfe74be965b86affea52670706e
rakibul114 <rakibulislam114@gmail.com>
2022-05-08 01:14:34 +0600

    implemented jwt for login system

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,24 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// verify jwt
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' });
+    }
+    console.log('decoded', decoded);
+    req.decoded = decoded;
+  });
+  
+  next();
+}
 
 // connection to MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2liqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -22,7 +41,19 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('warehouseManagement').collection('product');
-        const itemCollection = client.db("warehouseManagement").collection('item');
+      const itemCollection = client.db("warehouseManagement").collection('item');
+      
+
+      // For JWT (login)
+      // AUTH
+      app.post('/login', async (req, res) => {
+        const user = req.body;
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: '1d'
+        });
+        res.send({ accessToken });
+      });
+
 
 
         // API to get all products data
@@ -93,12 +124,18 @@ async function run() {
         });
 
         // API to get item
-        app.get('/item', async (req, res) => {
+      app.get('/item', verifyJWT, async (req, res) => {
+        const decodedEmail = req.decoded.email;
           const email = req.query.email;
-            const query = {email: email};
-            const cursor = itemCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
+        if (email === decodedEmail) {
+              const query = { email: email };
+              const cursor = itemCollection.find(query);
+              const items = await cursor.toArray();
+              res.send(items);
+        }
+        else {
+          res.status(403).send({ message: 'Forbidden access' });
+        }
         });      
       
 
